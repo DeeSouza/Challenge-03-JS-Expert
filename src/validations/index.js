287e6e91999c5ea650a7aab22d3945bf0801ee95
Diego Souza <dee-souza@hotmail.com>
2022-01-11 10:21:25 -0300

    fix export functions in file validations

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,13 +1,7 @@
-function checkAnswerEmpty(text) {
+export function checkAnswerEmpty(text) {
   return !text || text.length === 0;
 }
 
-function checkExpectationIsNumber(expectation) {
+export function checkExpectationIsNumber(expectation) {
   return isNaN(expectation) || !expectation;
 }
-
-module.exports = {
-  checkAnswerBeenComplete,
-  checkAnswerEmpty,
-  checkExpectationIsNumber,
-};
